395fea6039798e06e2b8c5d33308d785a974ca7a
Anthony Zardis <anthonyzardis@gmail.com>
2017-07-07 14:27:35 -0700

    Assignment-6-Reading-CSVs

--- address_bloc.rb
+++ address_bloc.rb
@@ -1,5 +1,5 @@
 require_relative 'controllers/menu_controllers'
-require_relative 'entries.csv'
+
 # #4 create new MenuController when AddressBloc starts
 menu = MenuController.new
 # #5 use system "clear" to clear command line 
@@ -7,4 +7,3 @@ system "clear"
 puts "Welcome to AddressBloc!"
 # #6 call main_menu to display menu
 menu.main_menu
-entries.csv
